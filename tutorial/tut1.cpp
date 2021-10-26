2bfd44375bc1d23cee1e7080c09a0d88ea154fb1
Arun Singh <arunsingh28aug.as@gmail.com>
2021-10-26 05:50:14 +0530

    refrence varible

---
+++ tutorial/tut1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int a, b;
+    cout << "enter value a:";
+    cin >> a;
+    cout << "enter value b:";
+    cin >> b;
+    cout << sizeof(a);
+    cout << "sum is:" << a + b;
+    return 0;
+}
